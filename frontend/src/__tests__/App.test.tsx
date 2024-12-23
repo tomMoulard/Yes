e039fcff0c1a0db250713b03e69f4e85d643240f
Tom Moulard <tom.moulard@perfectstay.com>
2024-12-24 00:25:09 +0100

    review

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import App from '../App';
 
 test('renders App component without crashing', () => {
